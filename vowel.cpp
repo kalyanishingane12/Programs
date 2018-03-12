590fceaf61314a6afdaea8242e33260878228cc3
kalyani shingane <36770973+kalyanishingane12@users.noreply.github.com>
2018-03-12 16:36:35 +0530

    Add files via upload

---
+++ vowel.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ int main()
+{
+   char ch;
+ cout<<"Enter an alphabet\n";
+  cin>>ch;
+       if(ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch=='u' || ch=='U')
+     {
+       cout<<"it is vowel"<<endl;
+       
+       }
+         
+     else
+     
+    {
+       cout<<"it is not vowel"<<endl;
+      }  
+     return 0;
+    } 

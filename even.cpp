c8487bb1be648c5b26eeae8c03b5619814937e0f
kalyani shingane <36770973+kalyanishingane12@users.noreply.github.com>
2018-03-12 16:38:03 +0530

    Add files via upload

---
+++ even.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i,n;
+  cout<<"Enter any number\n";
+  cin>>n;
+  if(n%2==0)
+  { 
+   cout<<"number is even";
+   }
+    else
+    { 
+    cout<<"number is odd";
+   }
+    return 0;
+    } 
+   
